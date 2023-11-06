5f097c26fc7019f08fdb76d338b0e62d465eea22
suryanshsinghnasahicker <suryanshsingh983810@gmail.com>
2023-11-06 23:17:23 +0530

    updated vrsn but with errors, v7

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,10 +1,11 @@
 import express  from 'express';
-import {registerController} from '../controllers/authController.js'
+import {registerController,loginController                          } from '../controllers/authController.js'
 //router object
 const router=express.Router()
 
 //routing
 //Register||Method POST
 router.post('/register',registerController)
-
+//login || Method Post 
+router.post('/login',loginController)
 export default router;
