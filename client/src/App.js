0d9eb428ce0d9447dcc6da9963d37be29d99bf7c
suryanshsinghnasahicker <suryanshsingh983810@gmail.com>
2023-12-22 19:36:49 +0530

    error fixed , nvm

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
+import React from 'react';
 import {Routes,Route} from 'react-router-dom'
 import Homepage from './pages/Homepage';
 function App() {
   return (
       <>
       <Routes>
-        <Route path='/' element={Homepage}/>
+        <Route path='/' element={<Homepage/>}/>
       </Routes>
     </>
   );
--- client/src/components/layout/Layout.js
+++ client/src/components/layout/Layout.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Foother from './Footer';
+import Footer from './Footer';
 import Header from './Header'
 const Layout=({children})=>{
 return(
