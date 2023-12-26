7bd5b7ee575d7564757e94b430270f360bf20668
suryanshsinghnasahicker <suryanshsingh983810@gmail.com>
2023-12-26 18:15:46 +0530

    updates

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,12 +1,15 @@
-import React from 'react';
-const Foother=()=>{
+import React from "react";
+import { Link } from "react-router-dom";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <h1 className="text-center">All Right Reserved &copy; Suryansh</h1>
+      <p className="text-center mt-3">
+        <Link to="/about">About</Link>|<Link to="/contact">Contact</Link>|
+        <Link to="/policy">Privacy Policy</Link>
+      </p>
+    </div>
+  );
+};
 
-    return(
-        <div className="bg-dark text-light p-3">
-            <h5 className="text-center">All Right Reserved &copy; Suryansh singh</h5>
-        </div>
-    )
-
-}
-
-export default Foother;
+export default Footer;
--- client/src/components/layout/Layout.js
+++ client/src/components/layout/Layout.js
@@ -1,16 +1,14 @@
-import React from 'react'
-import Footer from './Footer';
-import Header from './Header'
-const Layout=({children})=>{
-return(
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+const Layout = ({ children }) => {
+  return (
     <div>
-    <Header/>
-      <body style={{minHeight:'80vh'}}>  
-        {children}
-      </body>
-    <Footer/>
+      <Header />
+      <body style={{ minHeight: "80vh" }}>{children}</body>
+      <Footer />
     </div>
-)
-}
+  );
+};
 
 export default Layout;
