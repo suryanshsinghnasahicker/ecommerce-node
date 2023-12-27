1a0951c7b445936e6f313bfe4fa5165384d59917
suryanshsinghnasahicker <suryanshsingh983810@gmail.com>
2023-12-27 08:47:06 +0530

    fixed routing
    not sure if it is same as that but done

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import {Routes,Route} from 'react-router-dom'
-import Homepage from './pages/Homepage';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Contact from "./pages/Contact";
+import Policy from "./pages/Policy";
+import Pagenotfound from "./pages/PageNotFound";
+import About from "./pages/About";
 function App() {
   return (
-      <>
+    <>
       <Routes>
-        <Route path='/' element={<Homepage/>}/>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/policy" element={<Policy />} />
+        <Route path="*" element={<Pagenotfound />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </>
   );
--- client/src/components/layout/Layout.js
+++ client/src/components/layout/Layout.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
-const Layout = ({ children }) => {
+import Helmet from "react-helmet";
+const Layout = ({ children, title, description, keywords, author }) => {
   return (
     <div>
+      {/* <Helmet>
+        <meta charSet="utf-8" />
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+        <meta name="author" content={author} />
+        <title>{title}</title>
+      </Helmet> */}
       <Header />
-      <body style={{ minHeight: "80vh" }}>{children}</body>
+      <main style={{ minHeight: "80vh" }}>{children}</main>
       <Footer />
     </div>
   );
