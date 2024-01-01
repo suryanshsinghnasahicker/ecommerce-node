b549aab45f765590cba1485df22fc1a83a0a8ffb
suryanshsinghnasahicker <suryanshsingh983810@gmail.com>
2024-01-02 00:48:54 +0530

    debugged
    i feel so like god damn finally i did fix it, i do not know how name changed and after that second error was because of not ablet o extract env variables from private route, for now i has done it infront , not so secure , in future i will take care of it

--- client/src/components/Spinner.js
+++ client/src/components/Spinner.js
@@ -18,14 +18,17 @@ const Spinner = () => {
   return (
     <>
       <div
-        class="d-flex flex-column justify-content-center align-items-center"
+        className="d-flex flex-column justify-content-center align-items-center"
         style={{ height: "100vh" }}
       >
-        <h1 className="Text-center">
+        <h1
+          className="Tex
+t-center"
+        >
           Redirecting to login in {count} , you need to login first
         </h1>
-        <div class="spinner-border" role="status">
-          <span class="visually-hidden">Loading...</span>
+        <div className="spinner-border" role="status">
+          <span className="visually-hidden">Loading...</span>
         </div>
       </div>
     </>
--- client/src/components/routes/PrivateRoute.js
+++ client/src/components/routes/PrivateRoute.js
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react";
 import { useAuth } from "../../context/auth";
 import { Outlet } from "react-router-dom";
-import Spinner from "../Spinner";
 import axios from "axios";
+import Spinner from "../Spinner";
+// const myHost = import.meta.env.VITE_REACT_APP_API;
 
 export default function PrivateRoute() {
   const [ok, setOk] = useState(false);
@@ -10,14 +11,14 @@ export default function PrivateRoute() {
 
   useEffect(() => {
     const authCheck = async () => {
-      const res = axios.get(
-        "api/v1/auth/user-auth"
-        //   , {
-        //     headers: {
-        //       Aothorization: auth?.token,
-        //     },
-        //   }
-      );
+      const res = await axios.get(
+        `http://localhost:8080/api/v1/auth/user-auth`
+      ); //   , {
+      //     headers: {
+      //       Authorization: auth?.token,
+      //     },
+      //   }
+      console.log();
       if (res.data.ok) {
         setOk(true);
       } else {
