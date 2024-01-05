f8edd42c9ab0915b1f38d506b433750119e00915
suryanshsinghnasahicker <suryanshsingh983810@gmail.com>
2024-01-05 17:58:33 +0530

    product backend base
    did add all middle ware and basic structural base of back end of products, also done all POSTMAN testing , POSTMAN is real help white backend structure creation

---
+++ controllers/productController.js
@@ -0,0 +1,45 @@
+import productModel from "../models/productModel.js";
+import fs from "fs";
+import slugify from "slugify";
+
+export const createProductController = async (req, res) => {
+  try {
+    const { name, slug, description, price, category, quantity, shipping } =
+      req.fields;
+    const { photo } = req.files;
+    //validation
+    switch (true) {
+      case !name:
+        return res.status(500).send({ error: "name is required" });
+      case !description:
+        return res.status(500).send({ error: "description is required" });
+      case !price:
+        return res.status(500).send({ error: "price is required" });
+      case !category:
+        return res.status(500).send({ error: "category is required" });
+      case !quantity:
+        return res.status(500).send({ error: "quantity is required" });
+      case !photo && photo.size > 10000000:
+        return res
+          .status(500)
+          .send({ error: "photo is required and it gotta be less than 1 mb" });
+    }
+    const products = new productModel({ ...req.fields, slug: slugify(name) });
+    if (photo) {
+      products.photo.data = fs.readFileSync(photo.path);
+      products.photo.contentType = photo.type;
+    }
+    await products.save();
+    res.status(200).send({
+      success: true,
+      message: "product created successfully",
+      products,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "error in creating product ",
+    });
+  }
+};
