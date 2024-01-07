15106ac53d03d3597973ac95385b0e21138f6a2e
suryanshsinghnasahicker <suryanshsingh983810@gmail.com>
2024-01-07 17:21:26 +0530

    Update CreateCategory.js

--- client/src/pages/Admin/CreateCategory.js
+++ client/src/pages/Admin/CreateCategory.js
@@ -1,20 +1,28 @@
 import React, { useEffect, useState } from "react";
 import Layout from "../../components/layout/Layout";
 import AdminMenu from "../../components/layout/AdminMenu";
-import { Toast } from "react-toastify/dist/components";
-import axioss from "axios";
+import { toast } from "react-toastify";
+import axios from "axios";
 const CreateCategory = () => {
-  const [Category, setCategory] = useState([]);
+  const [categories, setCategories] = useState([]);
 
   //get all category
   const getAllCategory = async () => {
     try {
-      const { data } = await axios.get("/api/v1/category/get-category");
+      const { data } = await axios.get(
+        `${process.env.REACT_APP_API}/api/v1/category/get-category`
+      );
+      if (data.success) {
+        setCategories(data.category);
+      }
     } catch (error) {
       console.log(error);
-      Toast.error("something went wrong in getting category");
+      toast.error("something went wrong in getting category");
     }
   };
+  useEffect(() => {
+    getAllCategory();
+  }, []);
   return (
     <Layout title={"Dashboard-Create Category"}>
       <div className="container-fluid m-3 p-3">
@@ -23,7 +31,29 @@ const CreateCategory = () => {
             <AdminMenu />
           </div>
           <div className="col-md-9">
-            <h1>Create Category</h1>
+            <h1>Manage Category</h1>
+            <div className="w-75">
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {categories?.map((c) => (
+                    <>
+                      <tr>
+                        <td key={c._id}>{c.name}</td>
+                        <td>
+                          <button className="btn btn-primary"> Edit</button>
+                        </td>
+                      </tr>
+                    </>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
