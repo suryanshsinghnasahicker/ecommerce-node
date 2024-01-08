f1c514f2d29486175ab0690f2a4a2a6c878dfa1a
suryanshsinghnasahicker <suryanshsingh983810@gmail.com>
2024-01-08 23:43:07 +0530

    front-end,

--- client/src/pages/Admin/CreateCategory.js
+++ client/src/pages/Admin/CreateCategory.js
@@ -3,8 +3,30 @@ import Layout from "../../components/layout/Layout";
 import AdminMenu from "../../components/layout/AdminMenu";
 import { toast } from "react-toastify";
 import axios from "axios";
+import CategoryForm from "../../components/Form/CategoryForm";
 const CreateCategory = () => {
   const [categories, setCategories] = useState([]);
+  const [name, setName] = useState("");
+
+  //handle form
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post(
+        `${process.env.REACT_APP_API}/api/v1/category/create-category`,
+        { name }
+      );
+      if (data?.success) {
+        toast.success(`${name} is created`);
+        // getAllCategory();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("something went wrong in input form");
+    }
+  };
 
   //get all category
   const getAllCategory = async () => {
@@ -32,6 +54,13 @@ const CreateCategory = () => {
           </div>
           <div className="col-md-9">
             <h1>Manage Category</h1>
+            <div className="p-3 w-50">
+              <CategoryForm
+                handleSubmit={handleSubmit}
+                value={name}
+                setValue={setName}
+              />
+            </div>
             <div className="w-75">
               <table className="table">
                 <thead>
@@ -47,6 +76,10 @@ const CreateCategory = () => {
                         <td key={c._id}>{c.name}</td>
                         <td>
                           <button className="btn btn-primary"> Edit</button>
+                          <button className="btn btn-danger ms-2">
+                            {" "}
+                            Delete
+                          </button>
                         </td>
                       </tr>
                     </>
