6a1f20e2201531bd9b4a50903fece5f3dfb0bb85
suryanshsinghnasahicker <suryanshsingh983810@gmail.com>
2024-01-10 01:09:36 +0530

    single product page
    added uydate and dekte functions but has to do some debuggin in it

--- client/src/App.js
+++ client/src/App.js
@@ -20,6 +20,7 @@ import Users from "./pages/Admin/Users";
 import Orders from "./pages/user/Orders";
 import Profile from "./pages/user/Profile";
 import Product from "./pages/Admin/Product";
+import UpdateProduct from "./pages/Admin/UpdateProduct";
 function App() {
   return (
     <>
@@ -37,7 +38,9 @@ function App() {
           <Route path="admin" element={<AdminDashboard />} />
           <Route path="admin/create-category" element={<CreateCategory />} />
           <Route path="admin/create-product" element={<CreateProduct />} />
+          <Route path="admin/product/:slug" element={<UpdateProduct />} />
           <Route path="admin/products" element={<Product />} />
+
           <Route path="admin/users" element={<Users />} />
         </Route>
         <Route path="*" element={<Pagenotfound />} />
